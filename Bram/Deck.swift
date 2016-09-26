133c97c9cbb7084736ab96aeae54fac3c06cad92
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2016-09-26 17:32:17 +0800

    added deck

---
+++ Bram/Deck.swift
@@ -0,0 +1,39 @@
+//
+//  Deck.swift
+//  Bram
+//
+//  Created by Gabriel Gheorghian on 2016-09-26.
+//  Copyright © 2016 GabrielGhe. All rights reserved.
+//
+
+import UIKit
+import SwiftPriorityQueue
+
+class Deck {
+    private var mQueue: PriorityQueue<Card>
+    private let mName: String
+    
+    convenience init(name: String) {
+        self.init(name, [])
+    }
+    
+    init(_ name: String, _ cards: [Card]) {
+        self.mName = name
+        self.mQueue = PriorityQueue<Card>(ascending: false)
+        addCards(cards)
+    }
+    
+    func addCard(card: Card) {
+        addCards([card])
+    }
+    
+    func deleteCard(card: Card) {
+        mQueue.remove(card)
+    }
+    
+    private func addCards(cards: [Card]) {
+        for card in cards {
+            mQueue.push(card)
+        }
+    }
+}
